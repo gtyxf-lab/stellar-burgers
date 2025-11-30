2343c37dd05d90cfcc05a6cd8f8c71ef93b80339
gtyxf-lab <apa4a.r@yandex.ru>
2025-11-30 19:56:27 +0400

    fix constructor

--- src/components/ingredients-category/ingredients-category.tsx
+++ src/components/ingredients-category/ingredients-category.tsx
@@ -1,5 +1,6 @@
 import { TIngredient } from '@utils-types';
 import { forwardRef, useMemo } from 'react';
+import { useSelector } from '../../services/store';
 import { IngredientsCategoryUI } from '../ui/ingredients-category';
 import { TIngredientsCategoryProps } from './type';
 
@@ -8,23 +9,22 @@ export const IngredientsCategory = forwardRef<
   TIngredientsCategoryProps
 >(({ title, titleRef, ingredients }, ref) => {
   /** TODO: взять переменную из стора */
-  const burgerConstructor = {
-    bun: {
-      _id: ''
-    },
-    ingredients: []
-  };
+  const { bun, ingredients: constructorIngredients } = useSelector(
+    (state) => state.burgerConstructor
+  );
 
   const ingredientsCounters = useMemo(() => {
-    const { bun, ingredients } = burgerConstructor;
     const counters: { [key: string]: number } = {};
-    ingredients.forEach((ingredient: TIngredient) => {
+
+    constructorIngredients.forEach((ingredient: TIngredient) => {
       if (!counters[ingredient._id]) counters[ingredient._id] = 0;
       counters[ingredient._id]++;
     });
+
     if (bun) counters[bun._id] = 2;
+
     return counters;
-  }, [burgerConstructor]);
+  }, [bun, constructorIngredients]);
 
   return (
     <IngredientsCategoryUI
--- src/components/profile-menu/profile-menu.tsx
+++ src/components/profile-menu/profile-menu.tsx
@@ -1,11 +1,24 @@
-import { FC } from 'react';
-import { useLocation } from 'react-router-dom';
 import { ProfileMenuUI } from '@ui';
+import { FC } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { logoutUser } from '../../services/slices/user.slice';
+import { useDispatch } from '../../services/store';
 
 export const ProfileMenu: FC = () => {
   const { pathname } = useLocation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleLogout = () => {};
+  const handleLogout = () => {
+    dispatch(logoutUser())
+      .unwrap()
+      .then(() => {
+        navigate('/login');
+      })
+      .catch((error) => {
+        console.error('Ошибка при входе:', error);
+      });
+  };
 
   return <ProfileMenuUI handleLogout={handleLogout} pathname={pathname} />;
 };
